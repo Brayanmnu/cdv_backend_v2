e6406384b33510262f0709766657b65701d55be8
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-09-11 16:47:29 -0500

    Se agrega validacion de aforo y nro_doc

--- app/modules/registro_maker.py
+++ app/modules/registro_maker.py
@@ -39,50 +39,66 @@ class Maker (BaseModel):
 async def insert_maker(maker: Maker):
     try:
         
-        print(f'maker.iglesia: {maker.iglesia}')
-        
         conn = utils.conexion_postgres(host,port,db,usr,pwd)
         cursor = conn.cursor()
-        
-        insert_maker = "insert into makerv2(id_tipo_doc,nro_doc,nombres,apellidos,email,celular,fecha_creacion,fecha_actualizacion) values(%s,%s,%s,%s,%s,%s,current_timestamp,current_timestamp)  RETURNING id_makerv2"
-        cursor.execute(insert_maker,(maker.id_tipo_doc, maker.nro_doc, maker.nombre, maker.apellido, maker.email, maker.celular))
-        conn.commit()
-        print('Nuevo maker registrado')
-        if cursor.rowcount > 0:
-            dict_json_maker = cursor.fetchone()
-            id_maker = dict_json_maker[0]
+        query = "select count(1) from makerv2 m where nro_doc = %s"
+        cursor.execute(query,(maker.nro_doc,))
+        records = cursor.fetchone()
+        cant_nro_doc = records[0]
+        print(f'Query aforo ejecutado de disponibilidad: {cant_nro_doc}')
 
-            #Registrar a maker en evento
-            insert_evento_ciudad = "insert into maker_evento(id_makerv2,id_evento, ciudad, iglesia) values(%s,%s,%s,%s) RETURNING id"
-            cursor.execute(insert_evento_ciudad,(id_maker,maker.id_evento,maker.ciudad,maker.iglesia))
-            conn.commit()
-            print('Maker registrado a evento')
-            if cursor.rowcount > 0:
-                dict_json_evento = cursor.fetchone()
-                id_evento_maker = dict_json_evento[0]
+        if cant_nro_doc!=0:
+            dict_json = {"status":"doc_repetido"}
+        else:
+            query = "select cantidad-(select count(1) from makerv2 m ) as disponible from aforo"
+            
+            cursor.execute(query)
+            records = cursor.fetchone()
+            disponible = records[0]
+            print(f'Query aforo ejecutado de disponibilidad: {disponible}')
+            
+            
+            if disponible>0:
+                insert_maker = "insert into makerv2(id_tipo_doc,nro_doc,nombres,apellidos,email,celular,fecha_creacion,fecha_actualizacion) values(%s,%s,%s,%s,%s,%s,current_timestamp,current_timestamp)  RETURNING id_makerv2"
+                cursor.execute(insert_maker,(maker.id_tipo_doc, maker.nro_doc, maker.nombre, maker.apellido, maker.email, maker.celular))
+                conn.commit()
+                print('Nuevo maker registrado')
+                if cursor.rowcount > 0:
+                    dict_json_maker = cursor.fetchone()
+                    id_maker = dict_json_maker[0]
 
-                nombres_apellidos = maker.nombre +" " +maker.apellido
+                    #Registrar a maker en evento
+                    insert_evento_ciudad = "insert into maker_evento(id_makerv2,id_evento, ciudad, iglesia) values(%s,%s,%s,%s) RETURNING id"
+                    cursor.execute(insert_evento_ciudad,(id_maker,maker.id_evento,maker.ciudad,maker.iglesia))
+                    conn.commit()
+                    print('Maker registrado a evento')
+                    if cursor.rowcount > 0:
+                        dict_json_evento = cursor.fetchone()
+                        id_evento_maker = dict_json_evento[0]
 
-                #Generar codigoQR
-                url_qr = hosting
-                url_qr = str(url_qr)+ id_evento_maker
-                img = qrcode.make(url_qr)
-                print('qr generado correctamente')
-                img.save("qr_auxiliar.jpg")
-                with open("qr_auxiliar.jpg", "rb") as img_file:
-                    b64_string = base64.b64encode(img_file.read())
-                    b64_string = str(b64_string)
-                remove("qr_auxiliar.jpg")
-                client_mongo = MongoClient(uri_qr)
-                db_mongo = client_mongo[database_no_sql_qr]
-                collection_qr = db_mongo[collection_db_qr]
-                mydict = { "id_evento_maker": id_evento_maker, "b64_string": b64_string , "nombres_apellidos" : nombres_apellidos}
-                qr_result = collection_qr.insert_one(mydict)
-                print(f"Qr Guardado correctamente: {qr_result}")
-                client_mongo.close()
+                        nombres_apellidos = maker.nombre +" " +maker.apellido
 
-                dict_json = {"status":"ok", "codigo_qr":b64_string[2:-1] , "nombres_apellidos" : nombres_apellidos}
-    
+                        #Generar codigoQR
+                        url_qr = hosting
+                        url_qr = str(url_qr)+ id_evento_maker
+                        img = qrcode.make(url_qr)
+                        print('qr generado correctamente')
+                        img.save("qr_auxiliar.jpg")
+                        with open("qr_auxiliar.jpg", "rb") as img_file:
+                            b64_string = base64.b64encode(img_file.read())
+                            b64_string = str(b64_string)
+                        remove("qr_auxiliar.jpg")
+                        client_mongo = MongoClient(uri_qr)
+                        db_mongo = client_mongo[database_no_sql_qr]
+                        collection_qr = db_mongo[collection_db_qr]
+                        mydict = { "id_evento_maker": id_evento_maker, "b64_string": b64_string , "nombres_apellidos" : nombres_apellidos}
+                        qr_result = collection_qr.insert_one(mydict)
+                        print(f"Qr Guardado correctamente: {qr_result}")
+                        client_mongo.close()
+
+                        dict_json = {"status":"ok", "codigo_qr":b64_string[2:-1] , "nombres_apellidos" : nombres_apellidos}
+            else:
+                dict_json = {"status":"aforo"}
     except Exception as error:
         dict_json = {"status": "error"}
         print(f'Ocurrió un error inesperado,, cause: {error.__str__}')
@@ -92,4 +108,3 @@ async def insert_maker(maker: Maker):
             conn.close()
             print('conexion terminada')
     return dict_json
-
