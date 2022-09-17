1486551dd4844c94df5b4b444e6561b96a7d2892
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-09-17 13:29:50 -0500

    Se agrega nombre en asistencias

--- app/modules/asistencia.py
+++ app/modules/asistencia.py
@@ -15,18 +15,26 @@ class Asistencia(BaseModel):
 
 @router.get("/maker-evento/{item_id}")
 async def get_asistencias_by_maker_evento(item_id: str):
-    dict_json = []
     try:
         conn = utils.conexion_postgres(host,port,db,usr,pwd)
         cursor = conn.cursor()
-        select_query = "select row_to_json(row) from (select p.id as nro_ponencia from ponencia p inner join asistencia s on p.id=s.id_ponencia inner join maker_evento er on s.id_maker_evento = er.id where er.id=%s) row"
+        select_query = "select p.id as nro_ponencia, concat(m.nombres,' ',m.apellidos)as nombres  from ponencia p inner join asistencia s on p.id=s.id_ponencia inner join maker_evento er on s.id_maker_evento = er.id inner join makerv2 m on er.id_makerv2=m.id_makerv2 where er.id=%s"
         cursor.execute(select_query,(item_id,))
         conn.commit()
         print('Query ejecutado')
         if cursor.rowcount > 0:
             records = cursor.fetchall()
+            asistencias = []
             for row in records:
-                dict_json.append(row[0])
+                json_asistencia = {
+                    "nro_ponencia": row[0]
+                }
+                asistencias.append(json_asistencia)
+                nombres = row[1]
+            json_documento = {
+                "nombres":nombres,
+                "asistencias":asistencias
+            }
     except Exception as error:
         print(f'Ocurrió un error inesperado: {error.__str__}')
     finally:
@@ -34,7 +42,7 @@ async def get_asistencias_by_maker_evento(item_id: str):
             cursor.close()
             conn.close()
             print('conexion terminada')
-    return dict_json
+    return json_documento
 
 
 @router.post("/registrar")
