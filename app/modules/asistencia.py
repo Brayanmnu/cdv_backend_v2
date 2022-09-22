5d92adab871a1ed2c08de998237bbefa3dc6bd3e
Brayan Neyra <brayan.neyra@pragma.com.co>
2022-09-21 23:37:09 -0500

    Se agregan apis de permisos

--- app/modules/asistencia.py
+++ app/modules/asistencia.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import APIRouter
 from pydantic import BaseModel
 import app.utils as utils
@@ -13,12 +14,15 @@ class Asistencia(BaseModel):
     id_maker_evento: str
     id_ponencia: str
 
+
+class Permisos(BaseModel):
+    activos: str
+
 @router.get("/maker-evento/{item_id}")
 async def get_asistencias_by_maker_evento(item_id: str):
     try:
         conn = utils.conexion_postgres(host,port,db,usr,pwd)
         cursor = conn.cursor()
-        # select_query = "select p.id as nro_ponencia, concat(m.nombres,' ',m.apellidos)as nombres  from ponencia p inner join asistencia s on p.id=s.id_ponencia inner join maker_evento er on s.id_maker_evento = er.id inner join makerv2 m on er.id_makerv2=m.id_makerv2 where er.id=%s"
         select_query = "select p.id as nro_ponencia, concat(m.nombres,' ',m.apellidos)as nombres from makerv2 m left join maker_evento er on er.id_makerv2=m.id_makerv2  left join asistencia s on s.id_maker_evento = er.id  left join ponencia p on p.id = s.id_ponencia where er.id=%s"
         cursor.execute(select_query,(item_id,))
         conn.commit()
@@ -65,3 +69,58 @@ async def registrar_asistencia(asistencia: Asistencia):
             conn.close()
             print('conexion terminada')
     return dict_json
+
+
+
+@router.get("/permisos")
+async def get_asistencia_permisos():
+    json_arr=[]
+    try:
+        conn = utils.conexion_postgres(host,port,db,usr,pwd)
+        cursor = conn.cursor()
+        select_query = "select row_to_json(row) from (select id as nro_sticker from ponencia where estado='1') row"
+        cursor.execute(select_query)
+        conn.commit()
+        print('Query ejecutado')
+        if cursor.rowcount > 0:
+            records = cursor.fetchall()
+            for row in records:
+                json_arr.append(row[0])
+    except Exception as error:
+        print(f'Ocurrió un error inesperado: {error.__str__}')
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print('conexion terminada')
+    return json_arr
+
+
+
+
+@router.post("/permisos")
+async def actualizar_permisos(permisos: Permisos):
+    json_response= {'status': 'ok'}
+    try:
+        conn = utils.conexion_postgres(host,port,db,usr,pwd)
+        cursor = conn.cursor()
+        select_query = "update ponencia set estado = 0"
+        cursor.execute(select_query)
+        conn.commit()
+        print('Reset values')
+        for p in permisos.activos:
+            select_query = "update ponencia set estado = 1 where id=%s"
+            cursor.execute(select_query,(p,))
+            conn.commit()
+            print('actualizado correctamente')
+    except Exception as error:
+        print(f'Ocurrió un error inesperado: {error.__str__}')
+        json_response={
+            'status':'error'
+        }
+    finally:
+        if conn:
+            cursor.close()
+            conn.close()
+            print('conexion terminada')
+    return json_response
